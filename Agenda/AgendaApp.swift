eec9c815aaa1533a8d085e5015e7a04525ad9bbf
Isaí Arellano <isai.arellano@outlook.com>
2022-07-24 13:14:28 -0500

    Initial Commit

---
+++ Agenda/AgendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaApp.swift
+//  Agenda
+//
+//  Created by Isaí on 24/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AgendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Agenda/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Agenda
+//
+//  Created by Isaí on 24/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
